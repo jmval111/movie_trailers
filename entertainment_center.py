b8988aa71e74fca2922157679a221aab9f3de1dc
M. Val <jcutt11@yahoo.ca>
2018-03-06 22:02:53 -0500

    made line-length correctiosn as pre PEP8 guidelines

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,33 +4,45 @@ import media
 
 # Movie class instances
 joe_volcano = media.Movie("Joe Versus the Volcano",
-                          "A man quits his work life when he's misdiagnosed as dying."  # noqa,
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyMjU2OTM5OV5BMl5BanBnXkFtZTYwMDMyMzA5._V1_.jpg"  # noqa,
+                          "A man quits his work life when " +
+                          "he's misdiagnosed as dying.",
+                          "https://images-na.ssl-images-amazon.com" +
+                          "/images/M/MV5BMTMyMjU2OTM5OV5BMl5BanBnXkFtZTYwMDMyMzA5._V1_.jpg",
                           "https://www.youtube.com/watch?v=A9lceeNQMwk")
 
-akira = media.Movie("Akira", "A gang leader must save Tokyo from his transformed friend."  # noqa,
-                          "https://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg"  # noqa,
-                          "https://www.youtube.com/watch?v=-UhLderbuGI")
+akira = media.Movie("Akira", "A gang leader must save Tokyo " +
+                    "from his transformed friend.",
+                    "https://upload.wikimedia.org/wikipedia" +
+                    "/en/5/5d/AKIRA_%281988_poster%29.jpg",
+                    "https://www.youtube.com/watch?v=-UhLderbuGI")
 
 ghost_shell = media.Movie("Ghost in the Shell",
-                          "Man and machine meld in this brilliant futurescape."  # noqa,
-                          "https://www.movieposter.com/posters/archive/main/22/A70-11369"  # noqa,
+                          "Man and machine meld in this " +
+                          "brilliant futurescape.",
+                          "https://www.movieposter.com/posters/" +
+                          "archive/main/22/A70-11369  # noqa",
                           "https://www.youtube.com/watch?v=oP2Pt6m3yKU")
 
 powaqqatsi = media.Movie("Powaqqatsi",
-                          "Life is shown in elapsed tume."  # noqa,
-                          "https://upload.wikimedia.org/wikipedia/fi/1/14/Powaqqatsi-elokuvajuliste.jpg"  # noqa,
-                          "https://www.youtube.com/watch?v=fQxZ2oWvXB4")
+                         "Life is shown in elapsed time.",
+                         "https://upload.wikimedia.org/wikipedia/fi" +
+                         "/1/14/Powaqqatsi-elokuvajuliste.jpg",
+                         "https://www.youtube.com/watch?v=fQxZ2oWvXB4")
 
 koyaanisqatsi = media.Movie("Koyaanisqatsi",
-                          "Modern life is potrayed."  # noqa,
-                          "https://image.tmdb.org/t/p/w1280/oPr0osFyj81aGlRVQqhW7nq1D0a.jpg"  # noqa,
-                          "https://www.youtube.com/watch?v=1jM2WA2WbDc")
+                            "Modern life is " +
+                            "potrayed.",
+                            "https://image.tmdb.org/t/p/w1280/oPr0osF" +
+                            "yj81aGlRVQqhW7nq1D0a.jpg",
+                            "https://www.youtube.com/watch?v=1jM2WA2WbDc")
 
-odyssey = media.Movie("2001: A Space Odyssey",
-                          "An alien artifact communicates with humans throughout time."  # noqa,
-                          "http://www.reelviews.net/resources/img/posters/thumbs/2001_poster.jpg"  # noqa,
-                          "https://www.youtube.com/watch?v=XHjIqQBsPjk")
+odyssey = media.Movie("2001: A Space " +
+                      "Odyssey",
+                      "An alien artifact communicates " +
+                      "with humans throughout time.",
+                      "http://www.reelviews.net/resources/img/posters/" +
+                      "thumbs/2001_poster.jpg",
+                      "https://www.youtube.com/watch?v=XHjIqQBsPjk")
 
 movies = [joe_volcano, akira, ghost_shell, powaqqatsi, koyaanisqatsi, odyssey]
 fresh_tomatoes.open_movies_page(movies)
